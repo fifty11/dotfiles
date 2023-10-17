aa7e2c4e98497431e9aa4cd5abe7a08dbaf04f31
fifty11 <sixtenvolts@gmail.com>
2023-10-17 18:07:31 +0200

    icecreamsoogood

--- lua/thesixtendir/far.lua
+++ lua/thesixtendir/far.lua
@@ -1,8 +1,11 @@
+--made by fifty11 (@github)
+--i dont have a licence so it is ownerless :( but free to use for all use :)
 function far()
 	local find = vim.fn.input("find: ")
 	if find==nil then
 		return -1
 	end
+			find = find:gsub("%/", "\\/")
 	local replace = vim.fn.input("replace: ")
 	if replace==nil then
 		replace=""
